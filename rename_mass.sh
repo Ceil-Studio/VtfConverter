5991059b022106e663ccc511d701bd4b875273d2
Lucas Martins Lopes <ceiilciiuz@gmail.com>
2025-09-05 20:00:26 +0200

    Commit

---
+++ rename_mass.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Vérifier le nombre d'arguments
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <dossier> <ancienne_chaine> <nouvelle_chaine>"
+    exit 1
+fi
+
+dossier="$1"
+ancienne_chaine="$2"
+nouvelle_chaine="$3"
+
+# Vérifier si le dossier existe
+if [ ! -d "$dossier" ]; then
+    echo "Le dossier '$dossier' n'existe pas."
+    exit 1
+fi
+
+# Parcourir les fichiers .vmt dans le dossier donné
+for fichier in "$dossier"/*.vmt; do
+    if [ -f "$fichier" ]; then
+        echo "Traitement de $fichier..."
+        sed -i "s/$ancienne_chaine/$nouvelle_chaine/g" "$fichier"
+    fi
+done
+
+echo "Terminé !"
---
+++ vtf_to_jpeg.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Vérification si Wine est installé
+if ! command -v wine &>/dev/null; then
+  echo "Wine n'est pas installé ou n'est pas accessible dans le chemin."
+  exit 1
+fi
+
+# Vérification si le dossier est fourni en argument
+if [ -z "$1" ]; then
+  echo "Veuillez fournir le chemin du dossier contenant les images en argument."
+  exit 1
+fi
+
+# Vérification si le dossier existe
+if [ ! -d "$1" ]; then
+  echo "Le dossier spécifié n'existe pas."
+  exit 1
+fi
+
+# Se déplacer dans le dossier spécifié
+cd "$1" || exit 1
+
+# Boucle pour parcourir les fichiers d'image et exécuter la commande Wine pour chacun
+for image_file in *.jpeg; do
+  wine ./vtflib/bin/x64/VTFCmd.exe -file "$image_file" -format "dxt1"
+done
